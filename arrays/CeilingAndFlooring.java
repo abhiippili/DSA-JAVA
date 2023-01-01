8cb1442f85e690a689c6f504a02d3689ac722125
unknown <abhiippili1234@gmail.com>
2023-01-01 19:20:12 +0530

    added leetcode ques in ceiling and flooring

--- arrays/CeilingAndFlooring.java
+++ arrays/CeilingAndFlooring.java
@@ -4,10 +4,11 @@ public class CeilingAndFlooring {
     public static void main(String[] args) {
         int arr[] = {2,4,5,11,14,15,19,22};
         int target = 16;
-        int target1 = 24;
-        int target2 = 0;
         System.out.println(ceiling(arr,target));
         System.out.println(flooring(arr,target));
+        int arr1[] = {2,4,5,5,5,9,11,11,17,20,20,20,20};
+        int target1 = 20;
+        System.out.println(leetcodeCeil(arr1,target1));
     }
     public static int ceiling(int arr[], int target){
         if(target > arr[arr.length - 1])
@@ -41,4 +42,20 @@ public class CeilingAndFlooring {
         }
         return arr[end];
     }
+//    leetcode question -
+//        all the equal elements are to be skipped to right
+//        wrapping is also allowed
+    public static int leetcodeCeil(int arr[], int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(target >= arr[mid])
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return arr[start % arr.length];
+    }
 }
+
