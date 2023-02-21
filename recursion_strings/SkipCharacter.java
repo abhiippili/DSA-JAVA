aac7b105eef30a421baeacd08e1f121540eeb7a4
unknown <abhiippili1234@gmail.com>
2023-02-21 23:40:05 +0530

    recursion strings

--- recursion_strings/SkipCharacter.java
+++ recursion_strings/SkipCharacter.java
@@ -3,11 +3,29 @@ package com.recursion_strings;
 public class SkipCharacter {
     public static void main(String[] args) {
         String s = "baacad";
-
+        System.out.println(skip("", s));
+        System.out.println(skipString("", "ajghabhiihabhugabuabhirtyahia", "abhi"));
     }
-//    public static String skip(String up, String p){
-//        if(up.isEmpty())
-//            return p;
-//        if(up.charAt(0) == )
-//    }
+    public static String skip(String p, String up){
+        if(up.isEmpty()){
+            return p;
+        }
+        char ch = up.charAt(0);
+        if(ch == 'a'){
+            return skip(p, up.substring(1));
+        } else {
+            return skip(p + ch, up.substring(1));
+        }
+    }
+
+    public static String skipString(String p, String up, String s){
+        if(up.isEmpty()){
+            return p;
+        }
+        if(up.startsWith(s)){
+            return skipString(p, up.substring(s.length()), s);
+        } else {
+            return skipString(p + up.charAt(0), up.substring(1), s);
+        }
+    }
 }
